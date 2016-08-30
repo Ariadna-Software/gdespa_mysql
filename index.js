12a99b3a437f637d61079bbb74178f08ca07afb4
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-08-30 11:37:53 +0200

    fix(general): Authorize wasn't expose in general API

--- index.js
+++ index.js
@@ -5,12 +5,14 @@
 
 var dbCon = require('./lib/db_connection'),
     userGroup = require('./lib/user_group'),
-    user = require('./lib/user');
+    user = require('./lib/user'),
+    authorize= require('.lib/authorize');
 
 var app = {
 	dbCon: dbCon,
     user: user,
-    userGroup: userGroup
+    userGroup: userGroup,
+    authorize: authorize
 }
 
 module.exports = app;
